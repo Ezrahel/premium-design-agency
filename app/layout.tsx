b2188c89c8b9534aecd1de024d0b8e8c1cb7e4ef
ezrahel <adelakinisrael024@gmail.com>
2025-09-05 14:17:37 +0100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'F-TV',
+  title: 'FT-Vox',
   description: 'Built by Di-Tech',
 }
 
